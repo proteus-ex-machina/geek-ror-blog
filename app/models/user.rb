8785fa9c03cd3249141fb802c890de605946715c
proteus-ex-machina <ave.kolyanas@gmail.com>
2018-10-01 16:21:59 +0500

    Add callbacks for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,23 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
+  before_destroy :log_before_destroy
+  after_destroy :log_after_destroy
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :commented_posts, through: :comments, source: :commentable, source_type: :Post
   has_many :commented_users, through: :comments, source: :commentable, source_type: :User
 
   validates :name, presence: true
   validates :name, length: { maximum: 16, minimum: 2 }
   validates :name, uniqueness: true
+
+  private
+
+  def log_before_destroy
+    Rails.logger.info "##### Собираемся удалить пользователя #{name}. #####"
+  end
+
+  def log_after_destroy
+    Rails.logger.info "##### Пользователь #{name} удалён. #####"
+  end
 end
