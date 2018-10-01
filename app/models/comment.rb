6611cb4710fcae0fb7e4a8635f01547d52d58640
proteus-ex-machina <ave.kolyanas@gmail.com>
2018-10-01 13:30:00 +0500

    Change validations for post and comment models.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   validates :body, presence: true
-  validates :body, length: { maximum: 300, minimum: 1 }
+  validates :body, length: { maximum: 1000, minimum: 1 }
   validates :user, presence: true
   validates :post, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
-  validates :title, length: { maximum: 30, minimum: 1 }
+  validates :title, length: { maximum: 120, minimum: 1 }
   validates :title, uniqueness: true
   validates :body, presence: true
   validates :body, length: { maximum: 3000, minimum: 1}
